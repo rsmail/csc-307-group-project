6bd4974ecd009b206ee0f3d0b0bb2c7711ba5609
hoikinlam <hoikinlam1104@gmail.com>
2025-05-15 12:18:19 -0700

    TE4 Authentication

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,9 +1,26 @@
 // backend.js
+// note: npx nodemon backend.js (node backend.js for default)
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { registerUser } from "./auth.js";
+import { loginUser } from "./auth.js";
+import { authenticateUser } from "./auth.js";
+
+dotenv.config();
+// connect early - takes time + asynchronous
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+    .connect(MONGO_CONNECTION_STRING + "users") // connect to Db "users"
+    .catch((error) => console.log(error));
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -13,5 +30,20 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(
         `Example app listening at http://localhost:${port}`
+    );
+});
+
+app.get("/users", authenticateUser, (req, res) => {
+    const name = req.query.name;
+});
+
+app.post("/signup", registerUser);
+
+app.post("/login", loginUser);
+
+app.post("/users", authenticateUser, (req, res) => {
+    const userToAdd = req.body;
+    Users.addUser(userToAdd).then((result) =>
+        res.status(201).send(result)
     );
 });
--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,47 +1,209 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import Table from "./Table";
+import Form from "./Form";
+import Login from "./Login";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
-function App() {
-    const [count, setCount] = useState(0);
+function MyApp() {
+    const [characters, setCharacters] = useState([]);
+    const INVALID_TOKEN = "INVALID_TOKEN";
+    const [token, setToken] = useState(INVALID_TOKEN);
+    const [message, setMessage] = useState("");
+    const API_PREFIX = "http://localhost:8000";
+
+    function loginUser(creds) {
+        const promise = fetch(`${API_PREFIX}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(creds)
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    response
+                        .json()
+                        .then((payload) =>
+                            setToken(payload.token)
+                        );
+                    setMessage(
+                        `Login successful; auth token saved`
+                    );
+                } else {
+                    setMessage(
+                        `Login Error ${response.status}: ${response.data}`
+                    );
+                }
+            })
+            .catch((error) => {
+                setMessage(`Login Error: ${error}`);
+            });
+
+        return promise;
+    }
+
+    function signupUser(creds) {
+        const promise = fetch(`${API_PREFIX}/signup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(creds)
+        })
+            .then((response) => {
+                if (response.status === 201) {
+                    response
+                        .json()
+                        .then((payload) =>
+                            setToken(payload.token)
+                        );
+                    setMessage(
+                        `Signup successful for user: ${creds.username}; auth token saved`
+                    );
+                } else {
+                    setMessage(
+                        `Signup Error ${response.status}: ${response.data}`
+                    );
+                }
+            })
+            .catch((error) => {
+                setMessage(`Signup Error: ${error}`);
+            });
+
+        return promise;
+    }
+
+    function updateList(person) {
+        postUser(person)
+            .then((res) => {
+                if (res.status === 201) {
+                    return res.json();
+                }
+            })
+            .then((updatedPerson) => {
+                setCharacters([...characters, updatedPerson]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    function removeOneCharacter(index) {
+        fetch(`${API_PREFIX}/users/${characters[index]._id}`, {
+            method: "DELETE",
+            headers: addAuthHeader()
+        })
+            .then((res) => {
+                if (res.status === 204) {
+                    const updated = characters.filter(
+                        (character, i) => {
+                            return i !== index;
+                        }
+                    );
+                    setCharacters(updated);
+                }
+            })
+            .catch((error) => {
+                console.error(console.log(error));
+            });
+    }
+
+    function fetchUsers() {
+        return fetch(`${API_PREFIX}/users`, {
+            headers: addAuthHeader()
+        });
+    }
+
+    function addAuthHeader(otherHeaders = {}) {
+        if (token === INVALID_TOKEN) {
+            return otherHeaders;
+        } else {
+            return {
+                ...otherHeaders,
+                Authorization: `Bearer ${token}`
+            };
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers()
+            .then((res) =>
+                res.status === 200 ? res.json() : undefined
+            )
+            .then((json) => {
+                if (json) {
+                    setCharacters(json["users_list"]);
+                } else {
+                    setCharacters(null);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    function postUser(person) {
+        const promise = fetch(`${API_PREFIX}/users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(person)
+        });
+
+        return promise;
+    }
 
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img
-                        src={viteLogo}
-                        className="logo"
-                        alt="Vite logo"
+        <BrowserRouter>
+            <div className="container">
+                <nav>
+                    <Link to="/">Home</Link> |{" "}
+                    <Link to="/login">Login</Link> |{" "}
+                    <Link to="/signup">Signup</Link>
+                </nav>
+                <p>{message}</p>
+
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <Table
+                                    characterData={characters}
+                                    removeCharacter={
+                                        removeOneCharacter
+                                    }
+                                />
+                                <Form
+                                    handleSubmit={updateList}
+                                />
+                            </>
+                        }
                     />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
+                    <Route
+                        path="/login"
+                        element={
+                            <Login handleSubmit={loginUser} />
+                        }
                     />
-                </a>
+                    <Route
+                        path="/signup"
+                        element={
+                            <Login
+                                handleSubmit={signupUser}
+                                buttonLabel="Sign Up"
+                            />
+                        }
+                    />
+                </Routes>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button
-                    onClick={() =>
-                        setCount((count) => count + 1)
-                    }>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to
-                    test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        </BrowserRouter>
     );
 }
-
-export default App;
+export default MyApp;
